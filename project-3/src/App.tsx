0243f4bb7febbb5add68285c9484989244c11189
rapriasz <rapriasz@pgs-soft.com>
2021-07-24 15:37:12 +0200

    initial commit - project setup

---
+++ project-3/src/App.tsx
@@ -0,0 +1,114 @@
+import React, { useState } from "react";
+import { Layout, Menu, Breadcrumb } from "antd";
+import {
+  Form,
+  Input,
+  Button,
+  Radio,
+  Select,
+  Cascader,
+  DatePicker,
+  InputNumber,
+  TreeSelect,
+  Switch,
+} from "antd";
+import "./App.css";
+import { SizeType } from "antd/lib/config-provider/SizeContext";
+
+const { Content, Footer } = Layout;
+
+const App: React.FC = () => {
+  const [componentSize, setComponentSize] = useState<SizeType>("middle");
+
+  const onFormLayoutChange = ({ size }: any) => {
+    setComponentSize(size);
+  };
+
+  return (
+    <Layout className="layout">
+      <Content style={{ padding: "0 50px" }}>
+        <div className="site-layout-content">
+          <Form
+            labelCol={{
+              span: 4,
+            }}
+            wrapperCol={{
+              span: 14,
+            }}
+            layout="horizontal"
+            initialValues={{
+              size: componentSize,
+            }}
+            onValuesChange={onFormLayoutChange}
+            size={componentSize}
+          >
+            <Form.Item label="Form Size" name="size">
+              <Radio.Group>
+                <Radio.Button value="small">Small</Radio.Button>
+                <Radio.Button value="default">Default</Radio.Button>
+                <Radio.Button value="large">Large</Radio.Button>
+              </Radio.Group>
+            </Form.Item>
+            <Form.Item label="Input">
+              <Input />
+            </Form.Item>
+            <Form.Item label="Select">
+              <Select>
+                <Select.Option value="demo">Demo</Select.Option>
+              </Select>
+            </Form.Item>
+            <Form.Item label="TreeSelect">
+              <TreeSelect
+                treeData={[
+                  {
+                    title: "Light",
+                    value: "light",
+                    children: [
+                      {
+                        title: "Bamboo",
+                        value: "bamboo",
+                      },
+                    ],
+                  },
+                ]}
+              />
+            </Form.Item>
+            <Form.Item label="Cascader">
+              <Cascader
+                options={[
+                  {
+                    value: "zhejiang",
+                    label: "Zhejiang",
+                    children: [
+                      {
+                        value: "hangzhou",
+                        label: "Hangzhou",
+                      },
+                    ],
+                  },
+                ]}
+              />
+            </Form.Item>
+            <Form.Item label="DatePicker">
+              <DatePicker />
+            </Form.Item>
+            <Form.Item label="InputNumber">
+              <InputNumber />
+            </Form.Item>
+            <Form.Item label="Switch">
+              <Switch />
+            </Form.Item>
+            <Form.Item label="Button">
+              <Button>Button</Button>
+            </Form.Item>
+          </Form>
+        </div>
+      </Content>
+      <Footer style={{ textAlign: "center" }}>
+        Ant Design ©2018 Created by Ant UED
+      </Footer>
+    </Layout>
+  );
+};
+
+export default App;
